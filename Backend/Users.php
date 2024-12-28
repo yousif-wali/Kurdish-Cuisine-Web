460c682cf05301a1c182ffc82319de7b64073484
Yousif <wali.yousiff@gmail.com>
2024-12-28 21:53:33 +0300

    Modifying Users file, added (checkUser, delete, and selectUser) functions

--- Backend/Users.php
+++ Backend/Users.php
@@ -24,5 +24,51 @@ class Users{
             echo $e->getMessage();
         }
     }
+    // this function is used for login in users
+    public function checkUser(string $Username, string $Password){
+        try{
+            $queryString = "SELECT Password from `Users` WHERE Username = ?";
+            $result = $this->db->query($queryString, [$Username]);
+            // If error accures, it could be $result getting back different datatype
+            if(pssword_verify($Password, $result["Password"])){
+                echo "Success";
+            }else{
+                throw new Exception("Could not Sign in");
+            }
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    // This function is to delete a user.
+    public function delete(string $Username){
+        try{
+            $queryString = "DELETE FROM `Users` WHERE Username = ?";
+            $result = $this->db->query($queryString, [$Username]);
+
+            if($result){
+                echo "Success";
+            }else{
+                throw new Exception("Could not delete");
+            }
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    // This function retrieves data about users,
+    // Please note: we should have great security for this function
+    // As it is fetching critical data about our users.
+    private function selectUser(string $Username){
+        try{
+            $queryString = "SELECT * FROM `Users` WHERE Username = ?";
+            $result = $this->db->query($queryString, [$Username]);
+            if($result){
+                echo $result;
+            }else{
+                throw new Exception("Could not select any user.");
+            }
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+    }
 }
 ?>
